72cad79c6c034fffa6a04680cadbb781b2cc1f2d
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-06-18 03:51:06 +0300

    Make a hero-banner, body-part, and search-exercises components, and make a requist to the ExerciseDB API

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { Box } from "@mui/material";
+
+import HeroBanner from "../../components/hero-banner/hero-banner";
+import SearchExercises from "../../components/search-exercises/search-exercises";
+import Exercises from "../../components/exercises/exercises";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+    </Box>
+  );
 };
 
 export default Home;
