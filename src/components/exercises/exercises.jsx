fdfc7ad40435d73abcb77b25900e84722f5f0bac
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-06-18 20:18:30 +0300

    Finsh the Home page and exercuses component, and make exercise-card component with pagenation.

--- src/components/exercises/exercises.jsx
+++ src/components/exercises/exercises.jsx
@@ -1,5 +1,75 @@
-const Exercises = () => {
-  return <div>Exercises</div>;
+import { useState, useEffect } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Stack, Box, Typography } from "@mui/material";
+
+import { fetchData, exerciseOptions } from "../../utils/fetchData";
+import ExerciseCard from "../exercise-card/ExerciseCard";
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const fetchexercisesData = async () => {
+      let exercisesData = [];
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+      setExercises(exercisesData);
+    };
+    fetchexercisesData();
+  }, [bodyPart, setExercises]);
+
+  const exercisesPerPage = 9;
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercise = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (event, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  return (
+    <Box id="exercises" mt="50px" p="20px" sx={{ mt: { lg: "110px" } }}>
+      <Typography variant="h3" mb="46px">
+        Shoeing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {currentExercise.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > exercisesPerPage && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
+    </Box>
+  );
 };
 
 export default Exercises;
